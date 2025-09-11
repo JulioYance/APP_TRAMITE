623616101ed29c8ca901052eac375eabb241b234
Julio Yance Ramos <julioyanceramos@gmail.com>
2025-09-11 18:14:57 -0500

    asd

--- procesar_documentos.py
+++ procesar_documentos.py
@@ -1,7 +1,7 @@
 import os
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.docstore.document import Document
 from langchain_community.document_loaders import PyPDFLoader
 
